7199b589763461b111aee7c9bb6c586755fec2c7
Subalakshmi S <subavasagan@gmail.com>
2021-08-31 16:47:42 +0530

    Initial checkin

---
+++ src/testng/testngclass.java
@@ -0,0 +1,27 @@
+package testng;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class testngclass {
+
+	  @Test
+	  public void f() {
+		  System.out.println("Test part");
+	  }
+	  @BeforeTest
+	  public void beforeTest() {
+		  System.out.println("Before Test");
+		  
+	  }
+
+	  @AfterTest
+	  public void afterTest() {
+		  System.out.println("After test");
+	  }
+		
+
+	}
+
+
